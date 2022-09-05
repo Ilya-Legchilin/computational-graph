af590ef96b313dc857542d0124036bcca46e31b0
Ilya-Legchilin <legchilin.is@phystech.edu>
2022-09-05 22:48:10 +0300

    cache added

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 
 const NODE_VALUE_DEFAULT: f32 = 0f32;
 
@@ -13,15 +13,20 @@ struct Node {
     op: Operation,
     value: f32,
     edges: Vec<Rc<RefCell<Node>>>,
+    dependencies: Vec<Rc<RefCell<Node>>>,
+    cache: Vec<(Vec<f32>, f32)>,
 }
 
 impl Node {
     fn new(op: Operation) -> Rc<RefCell<Node>> {
-        Rc::new(RefCell::new(Node {
-            op: op,
+        let node = Rc::new(RefCell::new(Node {
+            op: op.clone(),
             value: NODE_VALUE_DEFAULT,
             edges: Vec::new(),
-        }))
+            dependencies: Vec::new(),
+            cache: Vec::new(),
+        }));
+        node
     }
 
     fn set(&mut self, value: f32) {
@@ -33,8 +38,12 @@ impl Node {
             return self.value;
         } else {
             match &self.op {
-                Operation::Mul => return self.edges[0].borrow().compute() * self.edges[1].borrow().compute(),
-                Operation::Add => return self.edges[0].borrow().compute() + self.edges[1].borrow().compute(),
+                Operation::Mul => {
+                    return self.edges[0].borrow().compute() * self.edges[1].borrow().compute()
+                }
+                Operation::Add => {
+                    return self.edges[0].borrow().compute() + self.edges[1].borrow().compute()
+                }
                 Operation::Sin => return self.edges[0].borrow().compute().sin(),
                 Operation::Pow(exp) => return self.edges[0].borrow().compute().powf(*exp),
                 _ => return self.value,
@@ -43,7 +52,7 @@ impl Node {
     }
 }
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone)]
 enum Operation {
     Input(String),
     Add,
@@ -61,6 +70,12 @@ fn pow_f32(input: Rc<RefCell<Node>>, exp: f32) -> Rc<RefCell<Node>> {
     {
         let mut node_borrowed = node.borrow_mut();
         node_borrowed.edges.push(input.clone());
+        match &input.borrow().op {
+            Operation::Input(_) => node_borrowed.dependencies.push(input.clone()),
+            _ => node_borrowed
+                .dependencies
+                .extend(input.borrow().dependencies.clone()),
+        };
     }
     node
 }
@@ -70,6 +85,12 @@ fn sin(input: Rc<RefCell<Node>>) -> Rc<RefCell<Node>> {
     {
         let mut node_borrowed = node.borrow_mut();
         node_borrowed.edges.push(input.clone());
+        match &input.borrow().op {
+            Operation::Input(_) => node_borrowed.dependencies.push(input.clone()),
+            _ => node_borrowed
+                .dependencies
+                .extend(input.borrow().dependencies.clone()),
+        };
     }
     node
 }
@@ -80,6 +101,18 @@ fn mul(input_one: Rc<RefCell<Node>>, input_two: Rc<RefCell<Node>>) -> Rc<RefCell
         let mut node_borrowed = node.borrow_mut();
         node_borrowed.edges.push(input_one.clone());
         node_borrowed.edges.push(input_two.clone());
+        match &input_one.borrow().op {
+            Operation::Input(_) => node_borrowed.dependencies.push(input_one.clone()),
+            _ => node_borrowed
+                .dependencies
+                .extend(input_one.borrow().dependencies.clone()),
+        };
+        match &input_two.borrow().op {
+            Operation::Input(_) => node_borrowed.dependencies.push(input_two.clone()),
+            _ => node_borrowed
+                .dependencies
+                .extend(input_two.borrow().dependencies.clone()),
+        };
     }
     node
 }
@@ -90,6 +123,18 @@ fn add(input_one: Rc<RefCell<Node>>, input_two: Rc<RefCell<Node>>) -> Rc<RefCell
         let mut node_borrowed = node.borrow_mut();
         node_borrowed.edges.push(input_one.clone());
         node_borrowed.edges.push(input_two.clone());
+        match &input_one.borrow().op {
+            Operation::Input(_) => node_borrowed.dependencies.push(input_one.clone()),
+            _ => node_borrowed
+                .dependencies
+                .extend(input_one.borrow().dependencies.clone()),
+        };
+        match &input_two.borrow().op {
+            Operation::Input(_) => node_borrowed.dependencies.push(input_two.clone()),
+            _ => node_borrowed
+                .dependencies
+                .extend(input_two.borrow().dependencies.clone()),
+        };
     }
     node
 }
@@ -102,30 +147,19 @@ fn main() {
     // graph variable is the output node of the graph:
     let graph = add(
         x1.clone(),
-        mul(
-            x2.clone(),
-            sin(
-                add(
-                    x2.clone(),
-                    pow_f32(
-                        x3.clone(),
-                        3f32
-                    )
-                )
-            )
-        )
+        mul(x2.clone(), sin(add(x2.clone(), pow_f32(x3.clone(), 3f32)))),
     );
     x1.borrow_mut().set(1f32);
     x2.borrow_mut().set(2f32);
     x3.borrow_mut().set(3f32);
-    let mut result = graph.borrow().compute();
+    let mut result = graph.borrow_mut().compute();
     result = round(result, 5);
     println!("Graph output = {}", result);
     assert_eq!(round(result, 5), -0.32727);
     x1.borrow_mut().set(2f32);
     x2.borrow_mut().set(3f32);
     x3.borrow_mut().set(4f32);
-    result = graph.borrow().compute();
+    result = graph.borrow_mut().compute();
     result = round(result, 5);
     println!("Graph output = {}", result);
     assert_eq!(round(result, 5), -0.56656);
@@ -138,12 +172,16 @@ mod test {
     #[test]
     fn node_new_test() {
         let node = Node::new(Operation::Input("x1".to_string()));
-        assert!(
-            match &node.borrow().op {
-                Operation::Input(name) => { if name == "x1" { true } else { false } },
-                _ => false,
+        assert!(match &node.borrow().op {
+            Operation::Input(name) => {
+                if name == "x1" {
+                    true
+                } else {
+                    false
+                }
             }
-        );
+            _ => false,
+        });
         assert!(node.borrow().value == NODE_VALUE_DEFAULT);
         assert!(node.borrow().edges.len() == 0);
     }
@@ -152,12 +190,16 @@ mod test {
     fn node_set_test() {
         let node = Node::new(Operation::Input("x1".to_string()));
         node.borrow_mut().set(10f32);
-        assert!(
-            match &node.borrow().op {
-                Operation::Input(name) => { if name == "x1" { true } else { false } },
-                _ => false,
+        assert!(match &node.borrow().op {
+            Operation::Input(name) => {
+                if name == "x1" {
+                    true
+                } else {
+                    false
+                }
             }
-        );
+            _ => false,
+        });
         assert!(node.borrow().value == 10f32);
         assert!(node.borrow().edges.len() == 0);
     }
@@ -171,22 +213,8 @@ mod test {
         let x5 = create_input("x5");
         // graph variable is the output node of the graph:
         let graph = add(
-            mul(
-                sin(
-                    x1.clone()
-                ),
-                pow_f32(
-                    x2.clone(),
-                    3f32,
-                )
-            ),
-            mul(
-                add(
-                    x3.clone(),
-                    x4.clone(),
-                ),
-                x5.clone(),
-            )
+            mul(sin(x1.clone()), pow_f32(x2.clone(), 3f32)),
+            mul(add(x3.clone(), x4.clone()), x5.clone()),
         );
         x1.borrow_mut().set(1f32);
         x2.borrow_mut().set(2f32);
@@ -210,24 +238,26 @@ mod test {
     #[test]
     fn create_input_test() {
         let input = create_input("ABCD");
-        assert!(
-            match &input.borrow().op {
-                Operation::Input(name) => { if name == "ABCD" { true } else { false } },
-                _ => false,
+        assert!(match &input.borrow().op {
+            Operation::Input(name) => {
+                if name == "ABCD" {
+                    true
+                } else {
+                    false
+                }
             }
-        );
+            _ => false,
+        });
     }
 
     #[test]
     fn pow_f32_test() {
         let x1 = create_input("x1");
         let node = pow_f32(x1.clone(), 10f32);
-        assert!(
-            match &node.borrow().op {
-                Operation::Pow(_exp) => true ,
-                _ => false,
-            }
-        );
+        assert!(match &node.borrow().op {
+            Operation::Pow(_exp) => true,
+            _ => false,
+        });
         assert!(node.borrow().value == NODE_VALUE_DEFAULT);
         assert!(node.borrow().edges.len() == 1);
     }
@@ -237,12 +267,10 @@ mod test {
         let x1 = create_input("x1");
         let x2 = create_input("x2");
         let node = add(x1.clone(), x2.clone());
-        assert!(
-            match &node.borrow().op {
-                Operation::Add => true ,
-                _ => false,
-            }
-        );
+        assert!(match &node.borrow().op {
+            Operation::Add => true,
+            _ => false,
+        });
         assert!(node.borrow().value == NODE_VALUE_DEFAULT);
         assert!(node.borrow().edges.len() == 2);
     }
@@ -252,12 +280,10 @@ mod test {
         let x1 = create_input("x1");
         let x2 = create_input("x2");
         let node = mul(x1.clone(), x2.clone());
-        assert!(
-            match &node.borrow().op {
-                Operation::Mul => true ,
-                _ => false,
-            }
-        );
+        assert!(match &node.borrow().op {
+            Operation::Mul => true,
+            _ => false,
+        });
         assert!(node.borrow().value == NODE_VALUE_DEFAULT);
         assert!(node.borrow().edges.len() == 2);
     }
@@ -266,12 +292,10 @@ mod test {
     fn sin_test() {
         let x1 = create_input("x1");
         let node = sin(x1.clone());
-        assert!(
-            match &node.borrow().op {
-                Operation::Sin => true ,
-                _ => false,
-            }
-        );
+        assert!(match &node.borrow().op {
+            Operation::Sin => true,
+            _ => false,
+        });
         assert!(node.borrow().value == NODE_VALUE_DEFAULT);
         assert!(node.borrow().edges.len() == 1);
     }
